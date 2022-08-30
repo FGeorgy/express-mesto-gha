978933af32f3c518a4d14f4f3fe0857897273b70
FGeaorg <g29f05@gmail.com>
2022-08-30 22:50:10 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -17,16 +17,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-
-  },
 });
 
 module.exports = mongoose.model('User', userSchema);
